e5b6762e0ebe732580f3ee2dda6f509a3707ca8b
mr-Deviant <Stanislav_Ostrovyi@EPUAKYIW0846.kyiv.epam.com>
2014-01-27 18:59:19 +0200

    Last commit

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -2,9 +2,20 @@
 
 angular.module('chatApp')
  	.controller('RegisterCtrl', function ($scope, $http) {
+
  		$scope.user = {};
 
-		$scope.register = function() {
+		$scope.register = function(form) {
+		
+			// Trigger validation flag.
+			$scope.submitted = true;
+
+			// If form is invalid, return and let AngularJS show validation errors.
+			if (form.$invalid) {
+				return;
+			}
+
+			// Submit form to server
 			$http({
 				method : 'POST',
 				url    : '/register',
